4823475343d496012aa9c196ff49660462f53b10
Smirnov Mihail <smirnov_mikhael@bk.ru>
2023-09-08 18:41:47 +0300

    Добавили файл ReadMe

--- src/Rewards/Diamond/DiamondRewards.java
+++ src/Rewards/Diamond/DiamondRewards.java
@@ -1,8 +1,16 @@
+/**
+ * Класс DiamondRewards представляет собой награду в виде бриллианта.
+ * Реализует интерфейс IGameItem для определения метода open(), который возвращает название награды.
+ */
 package Rewards.Diamond;
 
 import Abstractions.IGameItem;
 
 public class DiamondRewards implements IGameItem {
+    /**
+     * Метод open() возвращает название алмаз.
+     * @return Название алмаза ("Бриллиант").
+     */
     @Override
     public String open() {
         return "Бриллиант";
